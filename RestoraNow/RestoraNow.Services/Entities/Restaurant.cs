3fb2b6878437f0224a31d2b9368b4b230dd5137a
Ibrahim Topić <ibrahim.topic@edu.fit.ba>
2025-06-04 19:34:02 +0200

    Database setup #1

---
+++ RestoraNow/RestoraNow.Services/Entities/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoraNow.Services.Entities
+{
+    public class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
--- RestoraNow/RestoraNow.WebAPI/Program.cs
+++ RestoraNow/RestoraNow.WebAPI/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using RestoraNow.Services.Data;
+
 namespace RestoraNow.WebAPI
 {
     public class Program
@@ -13,6 +16,9 @@ namespace RestoraNow.WebAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
